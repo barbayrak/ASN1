082998963f706e5e90e176a86740cf1f243532d2
Kaan <kaanbarisbayrak@gmail.com>
2022-06-18 14:27:16 +0200

    Package name changes

--- Package.swift
+++ Package.swift
@@ -13,14 +13,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/barbayrak/BInt", from: "1.4.0"),
+		.package(url: "https://github.com/barbayrak/BInt", from: "2.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ASN1",
-            dependencies: ["BInt"]),
+            dependencies: [.product(name: "BInt", package: "BInt")]),
         .testTarget(
             name: "ASN1Tests",
             dependencies: ["ASN1"]),
